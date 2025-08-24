923bf35e6f030dc262159d965bfdfd84559a6360
merlinoalbus <69392236+merlinoalbus@users.noreply.github.com>
2025-08-24 18:10:07 +0200

    fix varie

--- server/server.js
+++ server/server.js
@@ -33,7 +33,8 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'http://localhost:3002',
-    'http://localhost'
+    'http://localhost',
+    'https://galaxytruckereditor.nasmerlinoalbus.cloud'
   ],
   credentials: true
 }));
